d6949cc70e22ba25bb976449e42949d8467b49b0
Suphanut Ngamgetsook <66070195@kmitl.ac.th>
2025-02-16 22:17:38 +0700

    add empty database and simple navigator bar

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const port = 3000;
+const sqlite3 = require('sqlite3').verbose();
+
+// Creating the Express server
+const app = express();
+
+// Connect to SQLite database
+let db = new sqlite3.Database('MyWebData.db', (err) => {    
+  if (err) {
+      return console.error(err.message);
+  }
+  console.log('Connected to the SQlite database.');
+});
+
+
+// static resourse & templating engine
+app.use(express.static('public'));
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+
+
+// routing path
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+
+// Starting the server
+app.listen(port, () => {
+   console.log("Server started.");
+ });

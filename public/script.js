09883c64fbe6898e897bbf5dd6d6a3ddbb8cbb9e
pckzy <sarun.debsirin@gmail.com>
2025-02-22 01:13:52 +0700

    add adduser page and some js

--- public/script.js
+++ public/script.js
@@ -79,3 +79,18 @@ $(document).ready(function() {
         "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "ทั้งหมด"]]
     });
 });
+
+
+document.getElementById('adduser-submit').addEventListener('click', function() {
+	if (document.getElementById('adduser-form').checkValidity()) {
+        document.getElementById('adduser-form').action = '/adduser-submit';
+        document.getElementById('adduser-form').submit();
+    } else {
+        document.getElementById('adduser-form').reportValidity();
+    }
+});
+
+document.getElementById('adduser-cancel').addEventListener('click', function() {
+	document.getElementById('adduser-form').action = '/adduser-cancel';
+	document.getElementById('adduser-form').submit();
+});
